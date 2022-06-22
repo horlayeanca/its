a9ee35b2833cf4f4221ec03ba6021228d3ab8670
horlayeanca <85955573+horlayeanca@users.noreply.github.com>
2022-06-22 12:54:19 +0100

    its_your_birthday

---
+++ index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    let text1 = 'Happy Birthday to you brother '
+    let text2 = 'Wish you many more beautiful years'
+    let text3 = text1 + text2
+    document.getElementById('demo').innerHTML = text3
+}
